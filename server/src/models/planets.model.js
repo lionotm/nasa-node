429f96cc0ce512a9e6dea26a8959e3681753da99
Lionel Ong <lion@Lion-MBA.local>
2021-07-11 17:27:31 +0800

    first commit

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+const parse = require("csv-parse");
+
+const planets = require("./planets.mongo");
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      // readable.pipe(writable)
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (data) => {
+        if (isHabitablePlanet(data)) {
+          await savePlanet(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+async function getAllPlanets() {
+  return await planets.find(
+    {},
+    {
+      // exclude these fields when calling MongoDB
+      __v: 0,
+      _id: 0,
+    }
+  );
+}
+
+async function savePlanet(planet) {
+  // insert + update = upsert, add only if it doesnt exist
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(`Could not save planet: ${err}`);
+  }
+}
+
+module.exports = {
+  loadPlanetsData,
+  getAllPlanets,
+};
---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,81 @@
+const request = require("supertest");
+const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+
+describe("Launches API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
+  });
+
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
+
+  describe("Test GET /launches", () => {
+    test("It should respond with 200 success", async () => {
+      const response = await request(app)
+        .get("/v1/launches")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
+
+  describe("Test POST /launch", () => {
+    const completeLaunchData = {
+      mission: "Enterprise",
+      rocket: "NCC 1431-A",
+      target: "Kepler-62 f",
+      launchDate: "March 12, 2030",
+    };
+
+    const LaunchDataWithoutDate = {
+      mission: "Enterprise",
+      rocket: "NCC 1431-A",
+      target: "Kepler-62 f",
+    };
+    const LaunchDataWithInvalidDate = {
+      mission: "Enterprise",
+      rocket: "NCC 1431-A",
+      target: "Kepler-62 f",
+      launchDate: "tucker",
+    };
+
+    test("It should respond with 201 created", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(completeLaunchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(responseDate).toBe(requestDate);
+
+      expect(response.body).toMatchObject(LaunchDataWithoutDate);
+    });
+
+    test("It should catch missing required properties", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(LaunchDataWithoutDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Missing required launch fields",
+      });
+    });
+
+    test("It should catch invalid dates", async () => {
+      const response = await request(app)
+        .post("/v1/launches")
+        .send(LaunchDataWithInvalidDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Invalid launch date",
+      });
+    });
+  });
+});
